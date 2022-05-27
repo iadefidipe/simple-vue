9e26a6f0c6c0fef48a785447b1bc88691f00a50c
Israel Adefidipe <israeladefidipe@gmail.com>
2022-05-27 02:19:21 +0100

    simple overview using cdn

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const app = Vue.createApp({
   // template: '<h1>{{firstName}}</h1>',
+  //data that you use in the app
   data() {
     return {
       firstName: "John",
@@ -8,6 +9,19 @@ const app = Vue.createApp({
       gender: "male",
       picture: "https://randomuser.me/api/portraits/men/10.jpg",
     }
+  },
+  //functions that you call in the components
+  methods: {
+    async getUser() {
+      const res = await fetch ('https://randomuser.me/api') 
+      const {results} = await res.json()
+
+      this.firstName = results[0].name.first
+      this.lastName= results[0].name.last
+      this.email= results[0].email
+      this.gender= results[0].gender
+      this.picture= results[0].picture.large
+    },
   },
 })
 
